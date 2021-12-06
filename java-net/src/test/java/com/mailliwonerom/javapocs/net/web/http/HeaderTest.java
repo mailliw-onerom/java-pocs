f062601cd188773e88615b0cb403b07f5e246fe0
William Moreno <mailliwonerom@gmail.com>
2021-12-06 18:10:01 -0300

    Header class tests finished.

--- java-net/src/test/java/com/mailliwonerom/javapocs/net/web/http/HeaderTest.java
+++ java-net/src/test/java/com/mailliwonerom/javapocs/net/web/http/HeaderTest.java
@@ -1,10 +1,12 @@
 package com.mailliwonerom.javapocs.net.web.http;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("--- Header ---")
 public class HeaderTest {
@@ -15,21 +17,35 @@ public class HeaderTest {
         header.add("Accept", "application/json");
 
         //The 'key' isn't needs be added because the header class ever uses two
-        //parameters.
+        //parameters when called from main method.
         assertThat(header.getValue()).contains("application/json");
     }
 
     @Test
+    public void addListOfValuesBasedOnSingleKey() {
+        assertThat(new Header("Accept", List.of("application/json",
+            "text/plain")).getValue()).contains("application/json", "text" +
+                "/plain");
+    }
+
+    @Test
     public void receiveAnExceptionIfOneOrBothParametersAreEmptyOrNull() {
-        Header header = new Header();
+        Header header = new Header("Accept", List.of("image/webp"));
 
         assertThrows(IllegalArgumentException.class, () -> {
             header.add("", "");
             header.add("", "application/json");
             header.add("Accept", "");
-            header.add(String.valueOf(null), "application/json");
+            header.add(String.valueOf(null), "application/xml");
             header.add("", String.valueOf(null));
             header.add(String.valueOf(null), String.valueOf(null));
+            header.add("Accept", List.of());
         });
+    }
+
+    @Test
+    public void parseIfParametersArentNullOrEmpty() {
+        assertThat(new Header().parse("Accept", List.of("text/plain",
+            "image/webp"))).isTrue();
     }
 }
