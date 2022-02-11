44d1675ecbf619652205caec2736647ac0a32351
William Moreno <mailliwonerom.contact@gmail.com>
2022-02-10 21:09:32 -0300

    Initial structure.

---
+++ java-jdbctemplate/src/test/java/com/mailliwonerom/javapocs/jdbctemplate/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mailliwonerom.javapocs.jdbctemplate;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+@DisplayName("--- Integration Test ---")
+public class ApplicationTests {
+
+    @Test
+    public void setUp() {
+    }
+}
